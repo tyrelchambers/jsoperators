363191a2c78d8166b81ae3b3e13d22be7fc99c1d
Tyrel Chambers <tychambers3@gmail.com>
2022-10-09 17:53:17 -0400

    update title

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "JSOperators",
   viewport: "width=device-width,initial-scale=1",
 });
 
